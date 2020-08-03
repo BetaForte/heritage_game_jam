49d601c39a230bc8d74aec5bfa553be3c01bd6f9
Forte <weiqiangchew@yahoo.com.sg>
2020-08-03 20:51:06 +0800

    Added Basic Menu Transition

---
+++ HGJ_Project/Assets/Scripts/MenuTransitionBasic.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuTransitionBasic : MonoBehaviour
+{
+    public GameObject nextPage;
+    public GameObject prevPage;
+    public List<GameObject> customPages = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void NextPage()
+    {
+        if(nextPage)
+        {
+            nextPage.SetActive(true);
+            gameObject.SetActive(false);
+        }
+    }
+
+    public void PrevPage()
+    {
+        if (prevPage)
+        {
+            prevPage.SetActive(true);
+            gameObject.SetActive(false);
+        }
+    }
+
+    public void CustomPage(int page)
+    {
+        if(customPages.Count > page && page >= 0 && customPages[page])
+        {
+            customPages[page].SetActive(true);
+            gameObject.SetActive(false);
+        }
+    }
+}
