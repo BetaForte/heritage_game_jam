fd693c334d9bc906e7ef45003d3a75dfadd11310
lucas <61584821+lucasyee999@users.noreply.github.com>
2020-08-05 22:58:50 +0800

    done

---
+++ HGJ_Project/Assets/Scripts/SetRoundTime.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetRoundTime : MonoBehaviour
+{
+    public float[] time;
+    public int index;
+
+    public Text currentTimeText;
+
+    private void Update()
+    {
+        ArenaSettings.instance.roundTime = time[index];
+
+        if(index == 0)
+            currentTimeText.text = "30 SECONDS";
+        if (index == 1)
+            currentTimeText.text = "1 MINUTE";
+        if (index == 2)
+            currentTimeText.text = "2 MINUTE";
+        if (index == 3)
+            currentTimeText.text = "3 MINUTE";
+        if (index == 4)
+            currentTimeText.text = "5 MINUTE";
+    }
+
+    public void OnRightButtonClicked()
+    {
+        if (index >= time.Length - 1)
+        {
+            index = 0;
+        }
+        else
+        {
+            index++;
+        }
+    }
+
+    public void OnLeftButtonClicked()
+    {
+        if (index <= 0)
+        {
+            index = time.Length - 1;
+        }
+        else
+        {
+            index--;
+        }
+    }
+
+}
