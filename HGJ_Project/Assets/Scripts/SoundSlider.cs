80688c0a2e19a92be453b527b5f4ce99655e3afe
Forte <weiqiangchew@yahoo.com.sg>
2020-08-06 20:35:39 +0800

    update mainmenu and new sounds

---
+++ HGJ_Project/Assets/Scripts/SoundSlider.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundSlider : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Slider slider;
+    Text text;
+    
+    void Start()
+    {
+        text = GetComponent<Text>();
+        UpdateSliderNumber();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateSliderNumber()
+    {
+        int percentage = (int)(slider.value / slider.maxValue * 100.0f);
+        text.text = percentage.ToString() + "%";
+    }
+}
